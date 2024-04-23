84531ff94490c0b53de4d06dc308db0c94431bcf
mastergroup <julipatepa@gmail.com>
2024-04-22 23:13:54 -0300

    subiendo archivos nuevo y enlazando seccion de botones del menu

--- script (1).js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const semanas = document.querySelectorAll(".semana");
-
-  semanas.forEach((semana, index) => {
-    setTimeout(() => {
-      semana.classList.add("active");
-    }, index * 1000);
-  });
-});
