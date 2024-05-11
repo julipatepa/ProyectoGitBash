22dd89b6b506c8192c055dc2d412bebfdf2aee5a
mastergroup <julipatepa@gmail.com>
2024-05-11 18:43:12 -0300

    cambiando arquitectura

--- proyecto.js
+++ proyecto.js
@@ -4,4 +4,23 @@ function enviarMensajeWhatsApp() {
     var url = "https://api.whatsapp.com/send?phone=" + telefono + "&text=" + encodeURIComponent(mensaje);
     window.open(url, "_blank");
 }
+document.addEventListener("DOMContentLoaded", function() {
+    
+    const primerasCajas = document.querySelectorAll('.cajas .caja');
+    const cajasNosotros = document.querySelectorAll('.caja-container');
+    function mostrarCajaConRetraso(caja, delay) {
+        setTimeout(() => {
+            caja.style.opacity = '1'; 
+            caja.style.transform = 'translateY(0)'; 
+        }, delay);
+    }
+    primerasCajas.forEach((caja, index) => {
+        const delay = index * 1000; 
+        mostrarCajaConRetraso(caja, delay);
+    });
 
+    cajasNosotros.forEach((caja, index) => {
+        const delay = index * 1000; 
+        mostrarCajaConRetraso(caja, delay);
+    });
+});
